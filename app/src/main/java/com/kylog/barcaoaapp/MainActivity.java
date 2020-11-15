a4acf98452fabd4d342d8c4d7d529da34eb4b0be
TJKylog <jesuus.23@gmail.com>
2020-11-14 18:54:09 -0600

    First commit

---
+++ app/src/main/java/com/kylog/barcaoaapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kylog.barcaoaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
