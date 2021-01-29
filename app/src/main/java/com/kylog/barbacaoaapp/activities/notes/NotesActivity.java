c4a0d57fb5b3984224c471e5f342fb57ba674102
TJKylog <jesuus.23@gmail.com>
2021-01-29 15:36:21 -0600

    Added print buetooth ticket

--- app/src/main/java/com/kylog/barbacaoaapp/activities/notes/NotesActivity.java
+++ app/src/main/java/com/kylog/barbacaoaapp/activities/notes/NotesActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
@@ -14,13 +15,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -58,12 +64,20 @@ import retrofit2.Response;
 public class NotesActivity extends AppCompatActivity {
 
     public static final int MESSAGE_STATE_CHANGE = 1;
-    public static final int MESSAGE_DEVICE_NAME = 4;
-    public static final String DEVICE_NAME = "device_name";
-    public static final int MESSAGE_TOAST = 5;
-    public static final String TOAST = "toast";
     public static final int MESSAGE_READ = 2;
     public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    public static final int MESSAGE_CONNECTION_LOST = 6;
+    public static final int MESSAGE_UNABLE_CONNECT = 7;
+    /*******************************************************************************************************/
+    // Key names received from the BluetoothService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
     private SharedPreferences pref;
     private RecyclerView typesList;
     private TypesAdapter typesAdapter;
@@ -86,9 +100,8 @@ public class NotesActivity extends AppCompatActivity {
     private TextView note_waiter_name;
     private BluetoothService mService;
     private Button print_ticket;
+    private String mConnectedDeviceName = null;
     BluetoothAdapter mBluetoothAdapter;
-    private ArrayAdapter<String> mPairedDevicesArrayAdapter;
-    private ArrayAdapter<String> mNewDevicesArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -188,11 +201,27 @@ public class NotesActivity extends AppCompatActivity {
         print_ticket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SendDataByte(Command.ESC_Init);
-                SendDataByte(Command.LF);
-                Print_Ex();
+                /*  */
+                if(mConnectedDeviceName == null) {
+                    Intent serverIntent = new Intent(NotesActivity.this, DeviceListActivity.class);
+                    startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+                }
+                else {
+                    SendDataByte(Command.ESC_Init);
+                    SendDataByte(Command.LF);
+                    Print_Ex();
+                }
             }
         });
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }
 
         get_types();
         get_active();
@@ -491,6 +520,22 @@ public class NotesActivity extends AppCompatActivity {
         });
     }
 
+    public void onStart() {
+        super.onStart();
+
+        // If Bluetooth is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the session
+        } else {
+            if (mService == null)
+                KeyListenerInit();//监听
+        }
+    }
+
     private void SendDataString(String data) {
 
         if (mService.getState() != BluetoothService.STATE_CONNECTED) {
@@ -508,6 +553,9 @@ public class NotesActivity extends AppCompatActivity {
         }
     }
 
+    /*
+     *SendDataByte
+     */
     private void SendDataByte(byte[] data) {
 
         if (mService.getState() != BluetoothService.STATE_CONNECTED) {
@@ -518,93 +566,145 @@ public class NotesActivity extends AppCompatActivity {
         mService.write(data);
     }
 
-    @SuppressLint("SimpleDateFormat")
-    private void Print_Ex(){
-
-
-            SimpleDateFormat formatter = new SimpleDateFormat ("yyyy/MM/dd/ HH:mm:ss ");
-            Date curDate = new Date(System.currentTimeMillis());//获取当前时间
-            String str = formatter.format(curDate);
-            String date = str + "\n\n\n\n\n\n";
-
-
-                try {
-                    byte[] qrcode = PrinterCommand.getBarCommand("Zijiang Electronic Thermal Receipt Printer!", 0, 3, 6);//
-                    Command.ESC_Align[2] = 0x01;
-                    SendDataByte(Command.ESC_Align);
-                    SendDataByte(qrcode);
-
-                    SendDataByte(Command.ESC_Align);
-                    Command.GS_ExclamationMark[2] = 0x11;
-                    SendDataByte(Command.GS_ExclamationMark);
-                    SendDataByte("NIKE Shop\n".getBytes("GBK"));
-                    Command.ESC_Align[2] = 0x00;
-                    SendDataByte(Command.ESC_Align);
-                    Command.GS_ExclamationMark[2] = 0x00;
-                    SendDataByte(Command.GS_ExclamationMark);
-                    SendDataByte("Number:  888888\nReceipt  S00003333\nCashier：1001\nDate：xxxx-xx-xx\nPrint Time：xxxx-xx-xx  xx:xx:xx\n".getBytes("GBK"));
-                    SendDataByte("Name    Quantity    price  Money\nShoes   10.00       899     8990\nBall    10.00       1599    15990\n".getBytes("GBK"));
-                    SendDataByte("Quantity：             20.00\ntotal：                16889.00\npayment：              17000.00\nKeep the change：      111.00\n".getBytes("GBK"));
-                    SendDataByte("company name：NIKE\nSite：www.xxx.xxx\naddress：ShenzhenxxAreaxxnumber\nphone number：0755-11111111\nHelpline：400-xxx-xxxx\n================================\n".getBytes("GBK"));
-                    Command.ESC_Align[2] = 0x01;
-                    SendDataByte(Command.ESC_Align);
-                    Command.GS_ExclamationMark[2] = 0x11;
-                    SendDataByte(Command.GS_ExclamationMark);
-                    SendDataByte("Welcome again!\n".getBytes("GBK"));
-                    Command.ESC_Align[2] = 0x00;
-                    SendDataByte(Command.ESC_Align);
-                    Command.GS_ExclamationMark[2] = 0x00;
-                    SendDataByte(Command.GS_ExclamationMark);
-
-                    SendDataByte("(The above information is for testing template, if agree, is purely coincidental!)\n".getBytes("GBK"));
-                    Command.ESC_Align[2] = 0x02;
-                    SendDataByte(Command.ESC_Align);
-                    SendDataString(date);
-                    SendDataByte(PrinterCommand.POS_Set_PrtAndFeedPaper(48));
-                    SendDataByte(Command.GS_V_m_n);
-                } catch (UnsupportedEncodingException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
-
-    }
-
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    /****************************************************************************************************/
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
         @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothService.STATE_CONNECTED:
+                            break;
+                        case BluetoothService.STATE_CONNECTING:
 
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                // If it's already paired, skip it, because it's been listed already
-                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-                    mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                }
-                // When discovery is finished, change the Activity title
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                setProgressBarIndeterminateVisibility(false);
-                setTitle("R.string.select_device");
-                if (mNewDevicesArrayAdapter.getCount() == 0) {
-                    String noDevices = "getResources().getText(R.string.none_found).toString()";
-                    mNewDevicesArrayAdapter.add(noDevices);
-                }
+                            break;
+                        case BluetoothService.STATE_LISTEN:
+                        case BluetoothService.STATE_NONE:
+
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:
+
+                    break;
+                case MESSAGE_READ:
+
+                    break;
+                case MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                    Toast.makeText(getApplicationContext(),
+                            "Connected to " + mConnectedDeviceName,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(),
+                            msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
+                            .show();
+                    break;
+                case MESSAGE_CONNECTION_LOST:    //蓝牙已断开连接
+                    Toast.makeText(getApplicationContext(), "Device connection was lost",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_UNABLE_CONNECT:     //无法连接设备
+                    Toast.makeText(getApplicationContext(), "Unable to connect device",
+                            Toast.LENGTH_SHORT).show();
+                    break;
             }
         }
     };
 
-    public String getLocalBluetoothName(){
-        if(mBluetoothAdapter == null){
-            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE: {
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    // Get the device MAC address
+                    String address = data.getExtras().getString(
+                            DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                    // Get the BLuetoothDevice object
+                    if (BluetoothAdapter.checkBluetoothAddress(address)) {
+                        BluetoothDevice device = mBluetoothAdapter
+                                .getRemoteDevice(address);
+                        // Attempt to connect to the device
+                        mService.connect(device);
+                    }
+                }
+                break;
+            }
+            case REQUEST_ENABLE_BT: {
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a session
+                    KeyListenerInit();
+                } else {
+                    // User did not enable Bluetooth or an error occured
+
+                    Toast.makeText(this, "xD",
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            }
         }
-        String name = mBluetoothAdapter.getName();
-        if(name == null){
-            System.out.println("Name is null!");
-            name = mBluetoothAdapter.getAddress();
+    }
+
+    /**
+     *
+     */
+    private void Print_Test(){
+    }
+
+    /**
+     *
+     */
+    @SuppressLint("SimpleDateFormat")
+    private void Print_Ex(){
+        SimpleDateFormat formatter = new SimpleDateFormat ("yyyy/MM/dd HH:mm:ss ");
+        Date curDate = new Date(System.currentTimeMillis());
+        String str = formatter.format(curDate);
+        String date = str + "\n\n\n\n\n\n";
+        try {
+
+            SendDataByte(Command.ESC_Align);
+            Command.GS_ExclamationMark[2] = 0x11;
+            SendDataByte(Command.GS_ExclamationMark);
+            SendDataByte("D'TOÑO\n".getBytes("GBK"));
+            Command.ESC_Align[2] = 0x00;
+            SendDataByte(Command.ESC_Align);
+            Command.GS_ExclamationMark[2] = 0x00;
+            SendDataByte(Command.GS_ExclamationMark);
+            SendDataByte("Receipt  S00003333\nCashier：1001\nDate：xxxx-xx-xx\nPrint Time：xxxx-xx-xx  xx:xx:xx\n".getBytes("GBK"));
+            SendDataByte("Name    Quantity    price  Money\nShoes   10.00       899     8990\nBall    10.00       1599    15990\n".getBytes("GBK"));
+            SendDataByte("Quantity：             20.00\ntotal：                16889.00\npayment：              17000.00\nKeep the change：      111.00\n".getBytes("GBK"));
+            SendDataByte("company name：NIKE\nSite：www.xxx.xxx\naddress：ShenzhenxxAreaxxnumber\nphone number：0755-11111111\nHelpline：400-xxx-xxxx\n================================\n".getBytes("GBK"));
+            Command.ESC_Align[2] = 0x01;
+            SendDataByte(Command.ESC_Align);
+            Command.GS_ExclamationMark[2] = 0x11;
+            SendDataByte(Command.GS_ExclamationMark);
+            SendDataByte("Welcome again!\n".getBytes("GBK"));
+            Command.ESC_Align[2] = 0x00;
+            SendDataByte(Command.ESC_Align);
+            Command.GS_ExclamationMark[2] = 0x00;
+            SendDataByte(Command.GS_ExclamationMark);
+
+            SendDataByte("(The above information is for testing template, if agree, is purely coincidental!)\n".getBytes("GBK"));
+            Command.ESC_Align[2] = 0x02;
+            SendDataByte(Command.ESC_Align);
+            SendDataString(date);
+            SendDataByte(PrinterCommand.POS_Set_PrtAndFeedPaper(48));
+            SendDataByte(Command.GS_V_m_n);
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        return name;
+    }
+
+    private void KeyListenerInit() {
+        mService = new BluetoothService(this, mHandler);
     }
 
     private String getToken(){
