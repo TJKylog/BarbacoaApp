e49c1e7174aa3e2c25b3d2c91d54bc774227fc93
TJKylog <jesuus.23@gmail.com>
2021-02-01 09:30:51 -0600

    Visual improvements

--- app/src/main/java/com/kylog/barbacaoaapp/RetrofitClient.java
+++ app/src/main/java/com/kylog/barbacaoaapp/RetrofitClient.java
@@ -25,7 +25,7 @@ public class RetrofitClient {
             httpClient.interceptors().add(logging);
 
             Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("http://192.168.1.76/")
+                    .baseUrl("http://api.encuadrenoticias.com.mx")
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .client(httpClient.build()).build();
 
