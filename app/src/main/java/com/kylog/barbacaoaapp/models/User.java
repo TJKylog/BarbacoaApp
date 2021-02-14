48cb8c971dffb5236ccc5b754ff859c7c76a09d0
TJKylog <jesuus.23@gmail.com>
2021-02-13 23:19:00 -0600

    new added lastname properties to user model

--- app/src/main/java/com/kylog/barbacaoaapp/models/User.java
+++ app/src/main/java/com/kylog/barbacaoaapp/models/User.java
@@ -14,6 +14,12 @@ public class User {
     @SerializedName("email")
     @Expose
     private String email;
+    @SerializedName("first_lastname")
+    @Expose
+    private String firstLastname;
+    @SerializedName("second_lastname")
+    @Expose
+    private String secondLastname;
     @SerializedName("role")
     @Expose
     private String role;
@@ -30,17 +36,21 @@ public class User {
 
     /**
      *
+     * @param firstLastname
      * @param role
      * @param roleId
+     * @param secondLastname
      * @param name
      * @param id
      * @param email
      */
-    public User(Integer id, String name, String email, String role, Integer roleId) {
+    public User(Integer id, String name, String email, String firstLastname, String secondLastname, String role, Integer roleId) {
         super();
         this.id = id;
         this.name = name;
         this.email = email;
+        this.firstLastname = firstLastname;
+        this.secondLastname = secondLastname;
         this.role = role;
         this.roleId = roleId;
     }
@@ -53,12 +63,22 @@ public class User {
         this.id = id;
     }
 
+    public User withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public User withName(String name) {
+        this.name = name;
+        return this;
     }
 
     public String getEmail() {
@@ -69,12 +89,48 @@ public class User {
         this.email = email;
     }
 
+    public User withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getFirstLastname() {
+        return firstLastname;
+    }
+
+    public void setFirstLastname(String firstLastname) {
+        this.firstLastname = firstLastname;
+    }
+
+    public User withFirstLastname(String firstLastname) {
+        this.firstLastname = firstLastname;
+        return this;
+    }
+
+    public String getSecondLastname() {
+        return secondLastname;
+    }
+
+    public void setSecondLastname(String secondLastname) {
+        this.secondLastname = secondLastname;
+    }
+
+    public User withSecondLastname(String secondLastname) {
+        this.secondLastname = secondLastname;
+        return this;
+    }
+
     public String getRole() {
         return role;
     }
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public User withRole(String role) {
+        this.role = role;
+        return this;
     }
 
     public Integer getRoleId() {
@@ -85,4 +141,8 @@ public class User {
         this.roleId = roleId;
     }
 
+    public User withRoleId(Integer roleId) {
+        this.roleId = roleId;
+        return this;
+    }
 }
